f70d60c4f6b3094005d20a5c6e86002f7d454255
Mike Elsmore <mike.elsmore@uk.ibm.com>
2017-03-04 02:52:05 +0000

    Add HTTPS force

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+// Force HTTPS
+if (location.protocol != 'https:') {
+ location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+// Materialize JS
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   $('.parallax').parallax();
