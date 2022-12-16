4099def258606143af80b2bb927eeb46c0aec332
Mike Elsmore <mike@elsmore.me>
2022-12-16 17:09:11 +0000

    It looks better now… sorry y'all

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Typography } from '@material-ui/core'
 import React from 'react'
-import Script from 'next/script'
 import Layout from '../components/layout/component'
+import { WebringComponent } from '../components/webring/component'
 
 export default function Home() {
   return (<Layout
@@ -11,8 +11,7 @@ export default function Home() {
     heroImageName="hackference-laugh"
   >
     <Typography>Mike Elsmore is a software developer, technologist, public speaker, and community builder who is passionate about developer outreach, education, and open source software. He is driven by the need to get involved with growing communities and with bringing technology to those with an interest or desire to learn. He has been involved in the Birmingham tech and hackathon scene for a long time, having founded //ackference and helped to spawn several other events.</Typography>
-    <the-claw-webring-widget theme="light" hideMembers="true"></the-claw-webring-widget>
-    <Script src="https://the-claw-webring-widget.netlify.app/the-claw-webring-widget.mjs" type="module" />
+    <WebringComponent/>
   </Layout>
   )
 }
