0ddde642ba0b2e75127e39ae3b7d45c05c0f5f82
Mike Elsmore <mike@elsmore.me>
2026-01-17 10:55:24 +0000

    Make the directus token optional

--- _data/directus.js
+++ _data/directus.js
@@ -1,8 +1,13 @@
 import 'dotenv/config'
 import { createDirectus, rest, staticToken } from '@directus/sdk';
 
-const directus = createDirectus(process.env.DIRECTUS_URL)
-    .with(staticToken(process.env.DIRECTUS_TOKEN))
-    .with(rest());
+let directus = createDirectus(process.env.DIRECTUS_URL).with(rest());
+
+// Add authentication if token is provided
+if (process.env.DIRECTUS_TOKEN) {
+    directus = createDirectus(process.env.DIRECTUS_URL)
+        .with(staticToken(process.env.DIRECTUS_TOKEN))
+        .with(rest());
+}
 
 export default directus;
